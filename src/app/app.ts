e45a4d81fe499bb295ff1241c5f60a9e84a45e97
nacerkraa <nacer.kraa@univ-constantine2.dz>
2025-09-01 22:32:51 +0100

    Initial Angular project

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('mini-shop');
+  protected readonly title = signal('zaki-shop');
 }
