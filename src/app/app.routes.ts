2caac5ee679e57bea4c63d9e59c36f890efd9564
nacerkraa <nacer.kraa@univ-constantine2.dz>
2025-09-01 23:07:38 +0100

    Set pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.js';
+import { ProductComponent } from './pages/product/product.js';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // default route
+  { path: 'product', component: ProductComponent }
+];
