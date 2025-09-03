d439f865328170abc59dda3bc78ef97d8279cf87
nacerkraa <nacer.kraa@univ-constantine2.dz>
2025-09-03 14:34:30 +0100

    minimize things

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { ProductDetailComponent } from './features/product-detail/product-detail.js';
-import { ProductComponent } from './pages/product/product.js';
 import { ProductListComponent } from './features/product-list/product-list.js';
 
 export const routes: Routes = [
   { path: '', component: ProductListComponent },
-  { path: 'product', component: ProductComponent },
   { path: 'product/:id', component: ProductDetailComponent },
   { path: '**', redirectTo: '' },
 ];
